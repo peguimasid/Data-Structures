83fc5b21069cbc716fde3bda7d25eea0f99e24ff
Guilhermo Masid <guilhermomasid@gmail.com>
2021-01-28 20:36:25 -0300

    Singly Linked List - Starter Code

---
+++ 02 -Singly Linked Lists/01 - startedCode.js
@@ -0,0 +1,23 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+
+  }
+}
+
+let list = new SinglyLinkedList()
+
+console.log(list)
+
