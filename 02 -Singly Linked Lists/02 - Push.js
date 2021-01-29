5b22a9bea828f0b01cb153ff7d8fba12e50d6ace
Guilhermo Masid <guilhermomasid@gmail.com>
2021-01-28 21:31:02 -0300

    Singly Linked List - push method

---
+++ 02 -Singly Linked Lists/02 - Push.js
@@ -0,0 +1,39 @@
+class Node {
+  constructor(val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val)
+
+    if(!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let list = new SinglyLinkedList()
+
+list.push('hi')
+list.push('there')
+list.push('my')
+// list.push('friend')
+
+console.log(list)
+
+
