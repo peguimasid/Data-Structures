1151e115317a9a8a54300881d64db82a32139a50
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-18 19:29:02 -0300

    push method

---
+++ 03 - Doubly Linked Lists/02 - Push.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val)
+
+    if(!this.length) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+}
+
+let list = new DoublyLinkedList()
+
+list.push(1)
+list.push(2)
+// list.push(3)
+
+console.log(list)
