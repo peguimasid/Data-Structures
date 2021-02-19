735837a2fde515e9e07c7f6c1242f562d1e9fc69
Guilhermo Masid <guilhermomasid@gmail.com>
2021-02-19 18:00:14 -0300

    pop method

---
+++ 03 - Doubly Linked Lists/03 - Pop.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    let newNode = new Node(val)
+
+    if(!this.length) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      this.tail.next = newNode
+      newNode.prev = this.tail
+      this.tail = newNode
+    }
+    this.length++
+    return this
+  }
+
+  pop() {
+    if(!this.length) return undefined
+
+    let removedNode = this.tail
+
+    if(this.length === 1) {
+      this.head = null
+      this.tail = null
+    } else {
+      this.tail = this.tail.prev
+      this.tail.next = null
+      removedNode.prev = null 
+    }
+    this.length--
+    return removedNode
+  }
+}
+
+let list = new DoublyLinkedList()
+
+list.push(1)
+list.push(2)
+list.push(3)
+list.push(4)
+
+list.pop()
+
+console.log(list)
