b0327d828a2c312e8c7e8391a04c6dbff398c9ca
Guilhermo Masid <guilhermomasid@gmail.com>
2021-03-02 18:03:46 -0300

    DLL Challange - Push

---
+++ 03 - Doubly Linked Lists/Challanges/push.js
@@ -0,0 +1,31 @@
+class Node{
+  constructor(val){
+      this.val = val;
+      this.next = null;      
+      this.prev = null;      
+  }
+}
+
+class DoublyLinkedList{
+  constructor(val){
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+  }
+  push(val){
+      let newNode = new Node(val);
+      if(this.length === 0) {
+          this.tail = newNode;
+          this.head = newNode;
+      } else {
+          newNode.prev = this.tail;
+          
+          this.tail.next = newNode;
+          this.tail = newNode;
+      }
+      
+      this.length++;
+      return this;
+  }
+  
+}
