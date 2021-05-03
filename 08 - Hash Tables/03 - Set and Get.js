59197a61456fa181b31d5a9cad4c47fb18952349
Guilhermo Masid <guilhermomasid@gmail.com>
2021-05-03 15:25:39 -0300

    Hash Tables - Set and Get

---
+++ 08 - Hash Tables/03 - Set and Get.js
@@ -0,0 +1,51 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size)
+  }
+
+  _hash(string) {
+    let result = 0
+    const PRIME_NUMBER = 31;
+    for (let i = 0; i < Math.min(string.length, 100); i++) {
+      const char = string[i]
+      const value = char.charCodeAt(0) - 96
+      result = (result * PRIME_NUMBER + value) % this.keyMap.length
+    }
+    return result
+  }
+
+  get(key) {
+    const position = this._hash(key)
+    const hashTable = this.keyMap
+
+    if (!hashTable[position]) return undefined
+
+    for (let i = 0; i < hashTable[position].length; i++) {
+      const itemKey = hashTable[position][i][0];
+
+      if (itemKey === key) return hashTable[position][i]
+    }
+
+    return undefined
+  }
+
+  set(key, value) {
+    const position = this._hash(key)
+
+    const hashTable = this.keyMap
+
+    hashTable[position] ?
+      hashTable[position].push([key, value]) :
+      hashTable[position] = [[key, value]]
+
+    return this.keyMap
+  }
+}
+
+const hashTable = new HashTable()
+
+hashTable.set('cyan', '#069')
+// hashTable.set('pinsssssdk', '#069')
+hashTable.set('pink', '#FFC0CB')
+
+console.log(hashTable.get('cyan'))
