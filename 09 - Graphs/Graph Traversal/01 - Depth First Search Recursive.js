034fe094e0945f373c43db3911e72a0a678f8570
Guilhermo Masid <guilhermomasid@gmail.com>
2021-06-09 17:05:02 -0300

    change file name

---
+++ 09 - Graphs/Graph Traversal/01 - Depth First Search Recursive.js
@@ -0,0 +1,71 @@
+class Graph {
+  constructor() {
+    this.adjacencyList = {};
+  }
+
+  addVertex(vertex) {
+    this.adjacencyList[vertex] = [];
+  }
+
+  addEdge(vertexOne, vertexTwo) {
+    this.adjacencyList[vertexOne].push(vertexTwo);
+    this.adjacencyList[vertexTwo].push(vertexOne);
+  }
+
+  removeEdge(vertexOne, vertexTwo) {
+    this.adjacencyList[vertexOne] = this.adjacencyList[vertexOne].filter(
+      (edge) => edge !== vertexTwo
+    );
+
+    this.adjacencyList[vertexTwo] = this.adjacencyList[vertexTwo].filter(
+      (edge) => edge !== vertexOne
+    );
+  }
+
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
+      const adjacentVertex = this.adjacencyList[vertex].pop();
+      this.removeEdge(vertex, adjacentVertex);
+    }
+
+    delete this.adjacencyList[vertex];
+  }
+
+  DepthFirstSearch(vertex) {
+    let list = this.adjacencyList;
+    let result = [];
+    let visited = {};
+
+    function traverse(vtx) {
+      if (!vtx.length) return;
+      result.push(vtx);
+      visited[vtx] = true;
+      for (let child of list[vtx]) {
+        if (!visited[child]) traverse(child);
+      }
+    }
+
+    traverse(vertex);
+
+    return result;
+  }
+}
+
+const graph = new Graph();
+
+graph.addVertex("A");
+graph.addVertex("B");
+graph.addVertex("C");
+graph.addVertex("D");
+graph.addVertex("E");
+graph.addVertex("F");
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("B", "D");
+graph.addEdge("C", "E");
+graph.addEdge("D", "E");
+graph.addEdge("D", "F");
+graph.addEdge("E", "F");
+
+console.log(graph.DepthFirstSearch("A"));
